1ef7f41ce7510c8101f41702e52a0b94d290d022
wooyong99 <wulovesk@naver.com>
2024-02-12 12:05:34 +0900

    feat : 템플릿 메서드 테스트

---
+++ src/test/java/spring/advance/trace/templatemethod/code/SubClassLogic2.java
@@ -0,0 +1,11 @@
+package spring.advance.trace.templatemethod.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class SubClassLogic2 extends AbstractTemplate {
+    @Override
+    protected void call() {
+        log.info("비즈니스 로직 실행2");
+    }
+}
